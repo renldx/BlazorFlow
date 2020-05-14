479c4ee8648181901dbac7390a8ff59e8601c635
Rene <renldx@gmail.com>
2020-05-13 23:11:58 -0400

    test

---
+++ BlazorFlow/Data/Flow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using QuikGraph;
+
+namespace BlazorFlow.Data
+{
+    public class Flow : AdjacencyGraph<FlowNode, FlowLink>
+    {
+        public Flow(int flowId, double flowVersion)
+        {
+            FlowId = flowId;
+            FlowVersion = flowVersion;
+        }
+        public int FlowId { get; set; }
+        public double FlowVersion { get; set; }
+    }
+}
---
+++ BlazorFlow/Data/FlowQuestion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorFlow.Data
+{
+    public class FlowQuestion
+    {
+        public FlowQuestion(int flowQuestionId, string flowQuestionCode, string flowQuestionTextEn, string flowQuestionTextFr)
+        {
+            FlowQuestionId = flowQuestionId;
+            FlowQuestionCode = flowQuestionCode;
+            FlowQuestionTextEn = flowQuestionTextEn;
+            FlowQuestionTextFr = flowQuestionTextFr;
+        }
+
+        public int FlowQuestionId { get; set; }
+        public string FlowQuestionCode { get; set; }
+        public string FlowQuestionTextEn { get; set; }
+        public string FlowQuestionTextFr { get; set; }
+    }
+}
