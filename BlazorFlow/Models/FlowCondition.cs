b22f4bf2a54b31ba9deb3c83a8e830536587dd22
Rene <renldx@gmail.com>
2020-05-26 19:32:01 -0400

    massive breaking changes part 2

--- BlazorFlow/Models/FlowCondition.cs
+++ BlazorFlow/Models/FlowCondition.cs
@@ -2,20 +2,30 @@ using System;
 
 namespace BlazorFlow.Models
 {
-    public interface IFlowCondition {}
+    public interface IFlowCondition
+    {
+        bool Evaluate();
+    }
 
     public class FlowCondition<T> : IFlowCondition where T : IComparable
     {
         private readonly Func<T, T, bool> operation;
         private readonly T requiredValue;
+        private T userValue;
 
         public FlowCondition(Func<T, T, bool> operation, T requiredValue)
         {
             this.operation = operation;
             this.requiredValue = requiredValue;
+            userValue = default(T);
         }
 
-        public bool Evaluate(T userValue)
+        public void SetUserValue(T userValue)
+        {
+            this.userValue = userValue;
+        }
+
+        public bool Evaluate()
         {
             if (userValue is {} uv && operation is {} op && requiredValue is {} rv)
             {
