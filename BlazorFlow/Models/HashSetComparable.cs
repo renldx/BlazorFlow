bd6504d73a470156f6eee24c0bf811eacc63a0bb
Rene <renldx@gmail.com>
2020-05-30 12:51:40 -0400

    implemented custom hash set type

---
+++ BlazorFlow/Models/HashSetComparable.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+namespace BlazorFlow.Models
+{
+    public class HashSetComparable<T> : HashSet<T>, IComparable<HashSet<T>>
+    {
+        public HashSetComparable() {}
+
+        public HashSetComparable(IEnumerable<T> collection) : base(collection) {}
+
+        public int CompareTo (HashSet<T>? hashSet)
+        {
+            if (hashSet is {} existing)
+            {
+                return SetEquals(hashSet) ? 0 : -1;
+            }
+            else {
+                return -1;
+            }
+        }
+    }
+}
